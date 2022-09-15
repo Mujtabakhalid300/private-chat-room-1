c9f964bd6d2942cbc1ce63ae2d83b5d1a27b2d69
Mujtaba <mujtabakhalid20@gmail.com>
2022-09-16 01:53:46 +0500

    added scrolltoview and styled chatbox

--- src/components/loginPage.js
+++ src/components/loginPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useState, useEffect } from "react";
 import sendMessage, { q } from "../firstore/firstoreFunctions";
 import { PropagateLoader } from "react-spinners";
@@ -9,6 +9,8 @@ import { onSnapshot, collection } from "firebase/firestore";
 const colRef = collection(db, "messages");
 
 const LoginPage = () => {
+  const scrollRef = useRef();
+  const scrollableDiv = useRef();
   const [loading, setLoading] = useState(false);
   const [name, setName] = useState("");
   const [message, setMessage] = useState("");
@@ -29,6 +31,12 @@ const LoginPage = () => {
 
   useEffect(() => {
     getChat();
+    setTimeout(() => {
+      scrollRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }, 1250);
   }, []);
 
   const handleSubmit = (e) => {
@@ -44,6 +52,12 @@ const LoginPage = () => {
         e.preventDefault();
         sendMessage("messages", name, message);
         setMessage("");
+        setTimeout(() => {
+          scrollRef.current.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+          });
+        }, 200);
       } else {
         alert("Can't send an empty message.");
         e.preventDefault();
@@ -57,7 +71,7 @@ const LoginPage = () => {
   return (
     <div className="main-container">
       <section className="section-1">
-        <h3 className="login-heading">Enter your name to continue</h3>
+        <h3 className="login-heading">Enter your name</h3>
         <form onSubmit={handleSubmit} className="form-1">
           <input
             className="login-input"
@@ -69,29 +83,34 @@ const LoginPage = () => {
         </form>
       </section>
 
-      {!loading ? (
-        <div className="chat-div">
-          {chat.map((e) => (
-            <div key={e.id} className="single-chat-div">
-              <h6 className="user-name-h1">{e.user}</h6>
-              <h5 className="user-message-h3">{e.message}</h5>
+      <section className="section-2">
+        {!loading ? (
+          <div className="chat-div">
+            <div className="messages-div" ref={scrollableDiv}>
+              {chat.map((e) => (
+                <div key={e.id} className="single-chat-div">
+                  <h6 className="user-name-h1">{e.user}</h6>
+                  <h5 className="user-message-h3">{e.message}</h5>
+                </div>
+              ))}
+              <div ref={scrollRef} className="focus"></div>
             </div>
-          ))}
-          <form onSubmit={handlemessage}>
-            <input
-              className="login-input"
-              value={message}
-              onChange={(e) => {
-                setMessage(e.target.value);
-              }}
-            />
-          </form>
-        </div>
-      ) : (
-        <div>
-          <PropagateLoader color="white" />
-        </div>
-      )}
+            <form className="chat-form" onSubmit={handlemessage}>
+              <input
+                className="login-input"
+                value={message}
+                onChange={(e) => {
+                  setMessage(e.target.value);
+                }}
+              />
+            </form>
+          </div>
+        ) : (
+          <div>
+            <PropagateLoader color="white" />
+          </div>
+        )}
+      </section>
     </div>
   );
 };
